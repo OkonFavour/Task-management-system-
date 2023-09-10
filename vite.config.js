c5857e7fff30c068a4e971c06c701554ea8ca21b
WillieWillie419 <nsikanokon13@gmail.com>
2023-09-10 19:01:57 +0100

    Blllll

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Task-management-system/",
+  base: "/Task-management-system-/",
   plugins: [react()],
 })
